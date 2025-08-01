73332a072664d02edab241ae1e09fbffe34239c4
Ravitheja <ravithejaravi14@gmail.com>
2025-08-01 22:31:36 +0530

    Affiliate website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import AffiliateWebsite from './components/AffiliateWebsite';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <AffiliateWebsite />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/products.js
@@ -0,0 +1,56 @@
+// products.js - Add your products here
+// Just add new objects to this array to display them on the website
+
+ const products = [
+  {
+    id: 1,
+    name: "NexLev LED Therapy Oil Applicator",
+    image: "https://m.media-amazon.com/images/I/51OgjXtg1mL._SL1280_.jpg",
+    affiliateUrl: "https://amzn.to/45vNNLp",
+  },
+  {
+    id: 2,
+    name: "Beardo Stress Buster Electric Scalp Massager",
+    image: "https://m.media-amazon.com/images/I/41FB8Bv4YoL.jpg",
+    affiliateUrl: "https://amzn.to/3U8GIdj",
+  },
+  {
+    id: 3,
+    name: "Ghar Soaps Sandalwood & Saffron",
+    image: "https://m.media-amazon.com/images/I/71zge8igCIL._SL1500_.jpg",
+    affiliateUrl: "https://amzn.to/4l6AMwk",
+  },
+  {
+    id: 4,
+    name: "ASUS Vivobook 15,13th Gen,Intel Core i5",
+    image: "https://m.media-amazon.com/images/I/71CgmbyKMFL._SL1500_.jpg",
+    affiliateUrl: "https://amzn.to/3HePSC6",
+  },
+  {
+    id: 5,
+    name: "Men's Cotton Blend Printed Straight Shirt",
+    image: "https://m.media-amazon.com/images/I/51qh1BgUMTL._SX569_.jpg",
+    affiliateUrl: "https://amzn.to/45wQuMM",
+  },
+  {
+    id: 6,
+    name: "rts Universal Travel Adapter",
+    image: "https://m.media-amazon.com/images/I/611RCy1XjsL._SL1500_.jpg",
+    affiliateUrl: "https://amzn.to/4mno3X5",
+  }
+];
+
+// To add new products, just copy this template and add it to the array above:
+/*
+{
+  id: 7, // increment the ID
+  name: "Your Product Name",
+  image: "https://your-image-url.jpg",
+  affiliateUrl: "https://your-affiliate-link.com",
+  price: "$XX.XX",
+  originalPrice: "$XX.XX", // optional
+  discount: "XX% OFF" // optional
+}
+*/
+
+export {products};
